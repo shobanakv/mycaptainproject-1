96c28f195bbc9baf17c9309e9200b66440fd99c7
shobanakv <90678251+shobanakv@users.noreply.github.com>
2022-03-04 22:41:40 +0530

    Add files via upload

---
+++ mycaptainproject---1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+from math import pi
+my_str = (input("Input the radius of the circle: "))
+print("Area of the circle with radius is: " + str(pi * r**2))
+
+
+# In[6]:
+
+
+filename = input("Input the Filename:  ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " , (f_extns[-1]))
+
